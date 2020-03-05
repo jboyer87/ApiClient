64db57473f19a08c9f70b7a227e8a3366e9f848e
James J. Boyer <james.boyer87@gmail.com>
2020-03-04 16:36:29 -0800

    Added basic connection class and a sample console app

---
+++ ApiClient.SampleApp/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using ApiClient.Common;
+
+namespace ApiClient.SampleApp
+{
+	class Program
+	{
+		static void Main()
+		{
+			Console.WriteLine("Hello World!");
+
+			string url = "https://postman-echo.com";
+			var responseType = ResponseType.PlainText;
+			var requestType = RequestType.Json;
+
+			var config = new BasicConnectionConfig(url, responseType, requestType);
+
+			var connection = new BasicConnection(config);
+
+			Response response = connection.Post("/post", null, "This is my body");
+
+			Console.WriteLine(
+				String.Format("The status code: {1}\nThe response: {0}", 
+				response.Message, 
+				response.StatusCode)
+			);
+		}
+	}
+}
